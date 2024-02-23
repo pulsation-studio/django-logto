8b870f8938f27b7543a809b01dbc4a192e4e3470
liam-pulsation <liam@pulsation.eco>
2024-02-23 15:36:06 +0100

    update README.md

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+with open('README.md', 'r') as file:
+    long_description = file.read()
 
 setup(
     name='django-logto',
-    version='1.0.1',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=[
         "django>=4.0",
@@ -16,5 +18,7 @@ setup(
     author="Pulsation Studio",
     author_email="liam@pulsation.eco",
     description="DRF and Django Logto integration",
-    url="https://github.com/pulsation-studio/django-logto"
+    url="https://github.com/pulsation-studio/django-logto",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
